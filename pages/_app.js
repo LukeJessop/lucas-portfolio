85612a39d5d91298e8f9944814ef9bdafa1ad6c8
LukeJessop <jessop.lucas@gmail.com>
2023-09-25 21:06:59 -0600

    hopefully fixed font issue for netlify

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Fragment, useEffect, useState } from "react";
 import PreLoader from "../src/layout/PreLoader";
 import "../styles/glitch.css";
 import "../styles/globals.css";
+import "../styles/plugins.css"
 import '../public/static/plugin/bootstrap/css/bootstrap.css';
 import '../public/static/plugin/font-awesome/css/all.css';
 import '../public/static/plugin/et-line/style.css';
@@ -11,6 +12,8 @@ import '../public/static/plugin/owl-carousel/css/owl.carousel.css';
 import '../public/static/plugin/magnific/magnific-popup.css';
 import '../public/static/plugin/scroll/jquery.mCustomScrollbar.css';
 import '../public/static/css/style.css';
+// import 'https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap'
+// import 'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
 
 
 function MyApp({ Component, pageProps }) {
