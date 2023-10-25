5c462ea11dec78b32a9d9951f1f6d8df2774d4de
LukeJessop <70823991+LukeJessop@users.noreply.github.com>
2023-10-25 13:42:52 -0600

    added my youtube channel videos to portfolio section

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'yt3.ggpht.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'i.ytimg.com',
+        },
+      ],
+    },
+  }
+  
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,25 +2,29 @@ import Isotope from "isotope-layout";
 import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import exploroImg from "./assets/exploro.jpeg";
+import pokemonFighters from "./assets/pokemonFighters.png";
+import axios from "axios";
 
 const portfolioData = [
   {
     className: "grid-item product website",
+    videoLink: "",
     title: "Exploro.com",
     category: "Travel Website",
     imgSrc: exploroImg,
     imgTitle: "exploro",
     imgAlt: "exploro",
-    link: "https://exploro.com",
+    link: "https://exploro.com"
   },
   {
     className: "grid-item product website",
+    videoLink: "",
     title: "Pokemon Fighters",
     category: "Video Game Website",
-    imgSrc: exploroImg,
-    imgTitle: "",
-    imgAlt: "",
-    link: "https://pokemonfighters.com", //look into possibly iframing these so they dont leave portfolio!
+    imgSrc: pokemonFighters,
+    imgTitle: "pokemonfighters",
+    imgAlt: "pokemon-fighters",
+    link: "https://pokemonfighters.com" //look into possibly iframing these so they dont leave portfolio!
   }
 ];
 
@@ -28,19 +32,20 @@ const Portfolio = () => {
   // Isotope
   const isotope = useRef();
   const [filterKey, setFilterKey] = useState("*");
+  const [youtubeVideos, setYoutubeVideos] = useState([]);
   useEffect(() => {
     isotope.current = new Isotope(".portfolio-content", {
       itemSelector: ".grid-item",
       //    layoutMode: "fitRows",
       percentPosition: true,
       masonry: {
-        columnWidth: ".grid-item",
+        columnWidth: ".grid-item"
       },
       animationOptions: {
         duration: 750,
         easing: "linear",
-        queue: false,
-      },
+        queue: false
+      }
     });
     return () => isotope.current.destroy();
   });
@@ -51,6 +56,43 @@ const Portfolio = () => {
         : isotope.current.arrange({ filter: `.${filterKey}` });
     }
   }, [filterKey]);
+
+  const fetchYoutubeData = async () => {
+    const apiKey = "AIzaSyDT1-mNDftguEgKRGbeeLlfkbxZgM9RuF0";
+
+    const channelId = "UChaN9QPqE1a5tqPCfbN3zGw";
+
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=10`,
+      { method: "GET" }
+    );
+    const data = response.json();
+    data.then((res) => {
+      setYoutubeVideos([...res.items]);
+      const formattedVideos = res.items
+        .filter((item) => item.snippet.title !== "Lucas Jessop")
+        .map((item) => {
+          console.log(item);
+          return {
+            className: "grid-item product video-editing",
+            videoLink: `https://www.youtube.com/watch?v=${item.id.videoId}`,
+            title: item.snippet.title,
+            category: item.snippet.description,
+            imgSrc: item.snippet.thumbnails.high.url,
+            imgTitle: "youtubeVideo",
+            imgAlt: "youtube-video",
+            link: `https://www.youtube.com/watch?v=${item.id.videoId}` //look into possibly iframing these so they dont leave portfolio!
+          };
+        });
+      portfolioData = [...portfolioData, ...formattedVideos];
+    });
+  };
+
+  useEffect(() => {
+    // Make a request to fetch channel videos
+    fetchYoutubeData();
+  }, []);
+
   const handleFilterKeyChange = (key) => () => {
     setFilterKey(key);
   };
@@ -101,21 +143,23 @@ const Portfolio = () => {
         <div className="portfolio-content grid-gutter-lg grid-col-3 lightbox-gallery">
           {portfolioData.map((item, index) => {
             return (
-              <a href={item.link} key={index}>
-                <div className={item.className}>
+              <a target="_blank" rel="noreferrer" href={item?.link} key={index}>
+                <div className={item?.className}>
                   <div className="portfolio-box-01">
                     <div className="portfolio-info">
                       <h5 className="white-color font-weight-bold">
-                        {item.title}
+                        {item?.title}
                       </h5>
-                      <span>{item.category}</span>
+                      <span>{item?.category}</span>
                     </div>
                     <div className="portfolio-img">
                       <Image
-                        src={item.imgSrc}
+                        src={item?.imgSrc}
+                        width={100}
+                        height={100}
                         layout="responsive"
-                        title={item.imgTitle}
-                        alt={item.imgAlt}
+                        title={item?.imgTitle}
+                        alt={item?.imgAlt}
                       />
                     </div>
                   </div>
